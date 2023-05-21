5b2b2b1bc21ca2bf07a6a61381e5266e4dea6d7d
Eduardo Campos <61758608+MAMBA48@users.noreply.github.com>
2023-05-21 14:39:34 -0300

    Add files via upload

--- index.jsx
+++ index.jsx
@@ -4,10 +4,11 @@ import { GlobalStyle } from './theme/GlobalStyle'
 import { Layout } from './components/layout'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
+  
   <React.StrictMode>
-    <GlobalStyle/>
-      <Layout>
-        
-      </Layout>
-  </React.StrictMode>,
+    <Layout>
+      <GlobalStyle/>
+
+    </Layout>
+  </React.StrictMode>
 )
